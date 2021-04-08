78aab742ef59c335eb5f00b77bc7e24fb8508b0e
Toerieeeeeee <112538@emmauscollege.nl>
2021-04-08 09:26:04 +0000

    05
    Signed-off-by: Toerieeeeeee <112538@emmauscollege.nl>

--- script.js
+++ script.js
@@ -68,24 +68,12 @@ var tekenVeld2 = function () {
  */
 var tekenMol = function(x, y) {
     fill("#A0522D")
-<<<<<<< HEAD
     ellipse(plaatsMolX, plaatsMolY,widthMol,heightMol) //hoofd mol
     fill("#241600") 
     ellipse(plaatsMolX, plaatsMolY, widthMol/5, heightMol/5) //neus mol
     fill("black")
     ellipse(plaatsMolX +20 ,plaatsMolY - 20, widthMol/6, heightMol/6) //rechter oog 
     ellipse(plaatsMolX -20,plaatsMolY -20, widthMol/6, heightMol/6) //linker oog 
-=======
-    ellipse(300,250,widthMol,heightMol) //hoofdmol
-    fill("#241600") 
-    ellipse(300,250, widthMol/5, heightMol/5) //neusmol
-    fill("black")
-    ellipse(320,230, widthMol/6, heightMol/6) //rechteroog 
-    ellipse(280,230, widthMol/6, heightMol/6) //linkeroog 
-    //line(290,280,310,280)
-    //arc(280,280,150,150,50,45)
-    //arc(150, 250, 150, 150, 1, 180)
->>>>>>> be060a0c2486c9012c455677172b7c63671a7def
 
 };
 
@@ -106,7 +94,6 @@ var tekenKogel = function(x, y) {
  * @param {number} y y-coördinaat
  */
 var tekenSpeler = function(x, y) {   
-    
    
 };
 
